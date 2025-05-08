1f1fc51c328cd5afd79fb9d9fe38a0921c11ece6
giniro12 <ilhammuhsin15@gmail.com>
2025-05-08 15:34:55 +0700

    kompeni web

---
+++ galeri.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modalImage');
+const images = document.querySelectorAll('.gallery-grid img');
+const closeBtn = document.querySelector('.close');
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) modal.style.display = 'none';
+});
